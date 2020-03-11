8e3b9481673c7e0bc07de9790de69576636757c1
Michele Barzan <michele@servizioglobale.it>
2020-03-11 16:32:14 +0100

    foto ordini

--- getFotoOrdine.php
+++ getFotoOrdine.php
@@ -1,30 +1,30 @@
 
 <?php
 
-include "connessione.php";
+    include "connessione.php";
 
-$ordine=$_REQUEST['ordine'];
-$stazione=$_REQUEST['stazione'];
+    $ordine=$_REQUEST['ordine'];
+    $stazione=$_REQUEST['stazione'];
 
-$fotoOrdine=[];
+    $fotoOrdine=[];
 
-$server_adress=$_SERVER['SERVER_ADDR'];
-$path="http://$server_adress/oasisfotoproduzione/FotoProduzioneAndroid/";
+    $server_adress=$_SERVER['SERVER_ADDR'];
+    $path="http://$server_adress/oasisfotoproduzione/FotoProduzioneAndroid/";
 
-$query2="SELECT DISTINCT dbo.registrazioni_produzione.ordine, dbo.registrazioni_produzione.stazione, REPLACE(REPLACE(dbo.allegati_registrazioni_produzione.percorso, '+', ''), '//srv-01/Produzione/', '') AS percorso FROM dbo.allegati_registrazioni_produzione INNER JOIN dbo.registrazioni_produzione ON dbo.allegati_registrazioni_produzione.registrazione_produzione = dbo.registrazioni_produzione.id_registrazione WHERE ordine='$ordine' AND stazione='$stazione'";	
-$result2=sqlsrv_query($conn,$query2);
-if($result2==TRUE)
-{
-    while($row2=sqlsrv_fetch_array($result2))
+    $query2="SELECT DISTINCT dbo.registrazioni_produzione.ordine, dbo.registrazioni_produzione.stazione, REPLACE(REPLACE(dbo.allegati_registrazioni_produzione.percorso, '+', ''), '//srv-01/Produzione/', '') AS percorso FROM dbo.allegati_registrazioni_produzione INNER JOIN dbo.registrazioni_produzione ON dbo.allegati_registrazioni_produzione.registrazione_produzione = dbo.registrazioni_produzione.id_registrazione WHERE ordine='$ordine' AND stazione='$stazione'";	
+    $result2=sqlsrv_query($conn,$query2);
+    if($result2==TRUE)
     {
-        $nomeFile=str_replace("//srv-dati/Produzione/FotoProduzioneAndroid/".$ordine."/".$stazione."/","",$row2['percorso']);
-        $foto["nomeFile"]=$nomeFile;
-        $foto["percorso"]=$path.$ordine."/".$stazione."/".$nomeFile;
-        array_push($fotoOrdine,$foto);
+        while($row2=sqlsrv_fetch_array($result2))
+        {
+            $nomeFile=explode($stazione."/",$row2['percorso'])[1];
+            $foto["nomeFile"]=$nomeFile;
+            $foto["percorso"]=$path.$ordine."/".$stazione."/".$nomeFile;
+            array_push($fotoOrdine,$foto);
+        }
+        echo json_encode($fotoOrdine);
     }
-    echo json_encode($fotoOrdine);
-}
-else
-    die("error");
+    else
+        die("error");
 
 ?>
