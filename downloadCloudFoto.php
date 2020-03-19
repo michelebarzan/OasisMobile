b6af378f8787aa0956f33d7dc10cb27b8dc8e506
Michele Barzan <michele@servizioglobale.it>
2020-03-19 10:52:44 +0100

    oasiscloudfoto v2

---
+++ downloadCloudFoto.php
@@ -0,0 +1,11 @@
+<?php
+
+    $src=$_GET["src"];
+
+    $file_url = $src;
+    header('Content-Type: application/octet-stream');
+    header("Content-Transfer-Encoding: Binary"); 
+    header("Content-disposition: attachment; filename=\"" . basename($file_url) . "\""); 
+    readfile($file_url); 
+
+?>
